6b9625b99778265937a662305e841f8104a061db
Vinodkumarseetha <77109811+Vinodkumarseetha@users.noreply.github.com>
2023-08-12 20:35:02 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,79 @@
+provider "aws" {
+  region = "us-west-2"  # Change to your desired region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone      = "us-west-2a"  # Change to your desired AZ
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone      = "us-west-2b"  # Change to your desired AZ
+}
+
+resource "aws_security_group" "allow_all" {
+  name_prefix = "allow_all_"
+
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.my_igw.id
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-03f65b8614a860c29"  # Change to your desired AMI
+  instance_type = "t2.micro"  # Change to your desired instance type
+  subnet_id     = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
